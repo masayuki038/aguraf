09a927aee3d4b44f15982b86c8a409fc866df4ba
Masayuki Takahashi <masayuki038@gmail.com>
2018-10-22 22:53:21 +0900

    Implement the getRowType function for converting from Parquet Type to RelDataType

--- src/main/scala/net/wrap_trap/aguraf/ParquetTranslatableTable.scala
+++ src/main/scala/net/wrap_trap/aguraf/ParquetTranslatableTable.scala
@@ -3,16 +3,21 @@ package net.wrap_trap.aguraf
 import java.lang.reflect.Type
 import java.nio.file.{Files, Path}
 
+import org.apache.calcite.rel.`type`.RelDataTypeFactory.FieldInfoBuilder
+import org.apache.calcite.sql.`type`.SqlTypeName
+import org.apache.parquet.column.ColumnDescriptor
+import org.apache.parquet.hadoop.ParquetFileReader
+import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName
+
 import collection.JavaConverters._
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.calcite.adapter.java.JavaTypeFactory
 import org.apache.calcite.linq4j.tree.Expression
 import org.apache.calcite.linq4j.{Queryable, QueryProvider}
 import org.apache.calcite.plan.RelOptTable
 import org.apache.calcite.plan.RelOptTable.ToRelContext
 import org.apache.calcite.rel.RelNode
-import org.apache.calcite.rel.`type`.{RelProtoDataType, RelDataType, RelDataTypeFactory}
+import org.apache.calcite.rel.`type`.{RelDataType, RelDataTypeFactory}
 import org.apache.calcite.schema.impl.AbstractTable
 import org.apache.calcite.schema.{SchemaPlus, TranslatableTable, QueryableTable}
 import org.apache.hadoop.conf.Configuration
@@ -20,31 +25,46 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{Path => HDFSPath}
 import org.apache.parquet.format.converter.ParquetMetadataConverter
 
-class ParquetTranslatableTable(val tableName: String, val dirPath: Path, val tProtoRowType: RelProtoDataType)
-  extends AbstractTable with QueryableTable with TranslatableTable with LazyLogging {
+object ParquetTranslatableTable extends LazyLogging {
 
-  val columns = {
+  def apply(tableName: String, columns: Seq[ColumnDescriptor]): ParquetTranslatableTable = {
+    new ParquetTranslatableTable(tableName, columns)
+  }
+
+  def apply(tableName: String, dirPath: Path): ParquetTranslatableTable = {
+    new ParquetTranslatableTable(tableName, getColumns(dirPath))
+  }
+
+  private def getColumns(dirPath: Path): Seq[ColumnDescriptor] = {
     val parquetFiles = Files.newDirectoryStream(dirPath, "*.parquet").asScala.toSeq
     val hdfsPath = new HDFSPath(parquetFiles(0).toAbsolutePath().toString())
     val config = new Configuration()
     logger.debug(s"Reading Parquet schema from $dirPath")
-    val metadata = org.apache.parquet.hadoop.ParquetFileReader.readFooter(config, hdfsPath, ParquetMetadataConverter.NO_FILTER)
+    val metadata = ParquetFileReader.readFooter(config, hdfsPath, ParquetMetadataConverter.NO_FILTER)
     val messageType = metadata.getFileMetaData.getSchema()
-    messageType.getColumns
+    messageType.getColumns.asScala
     // TODO Check all parquet files have same column descriptors
   }
+}
 
-  def getRowType(typeFactory: RelDataTypeFactory): RelDataType = {
-    if (this.tProtoRowType != null) {
-      return this.tProtoRowType.apply(typeFactory)
+class ParquetTranslatableTable(val tableName: String, val columns: Seq[ColumnDescriptor])
+  extends AbstractTable with QueryableTable with TranslatableTable with LazyLogging {
+
+  override def getRowType(typeFactory: RelDataTypeFactory): RelDataType = {
+    val fieldBuilder = new FieldInfoBuilder(typeFactory)
+    columns.foreach { descriptor =>
+      assert(descriptor.getPath.size == 1)
+      val name = descriptor.getPath.apply(0).toUpperCase
+      val columnType = descriptor.getType match {
+        case PrimitiveTypeName.FLOAT => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.FLOAT), true)
+        case PrimitiveTypeName.BINARY => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.VARCHAR), true)
+        case PrimitiveTypeName.INT32 => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.INTEGER), true)
+        case PrimitiveTypeName.INT64 => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.BIGINT), true)
+        case PrimitiveTypeName.DOUBLE => typeFactory.createTypeWithNullability(typeFactory.createSqlType(SqlTypeName.DOUBLE), true)
+      }
+      fieldBuilder.add(name, columnType)
     }
-    deduceRowType(null, typeFactory.asInstanceOf[JavaTypeFactory])
-  }
-
-  def deduceRowType(a: ParquetSchema, typeFactory: JavaTypeFactory): RelDataType = {
-    // parquetのSchemaをreadして、それに対する(fieldName, RelDataType)を返す
-    // githubにあるcalcite+parquetのものを参考にする
-    return null
+    fieldBuilder.build()
   }
 
   override def getExpression(schema: SchemaPlus, tableName: String, clazz: Class[_]): Expression = ???
--- src/test/scala/net/wrap_trap/aguraf/ParquetTranslatableTableSpec.scala
+++ src/test/scala/net/wrap_trap/aguraf/ParquetTranslatableTableSpec.scala
@@ -1,6 +1,11 @@
 package net.wrap_trap.aguraf
 
 import java.nio.file.Paths
+import org.apache.calcite.jdbc.JavaTypeFactoryImpl
+import org.apache.calcite.sql.`type`.{SqlTypeName, BasicSqlType}
+import org.apache.parquet.column.ColumnDescriptor
+import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName
+
 import collection.JavaConverters._
 
 import org.scalatest.FunSpec
@@ -11,18 +16,45 @@ import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName._
 class ParquetTranslatableTableSpec extends FunSpec {
 
   describe("ParquetTranslatableTable") {
-    it ("should get columns in parquet files") {
+    it("should get columns in parquet files") {
       val dirPath = Paths.get("src/test/resources/samples").toAbsolutePath()
-      val columns = new ParquetTranslatableTable("SAMPLES", dirPath, null).columns.asScala
+      val columns = ParquetTranslatableTable("SAMPLES", dirPath).columns
       val paths = columns.map(_.getPath).flatten
-      paths should contain ("unionCol")
-      paths should contain ("simpleCol")
+      paths should contain("unionCol")
+      paths should contain("simpleCol")
       columns.filter(c => c.getPath().contains("unionCol")).foreach {
-        _.getPrimitiveType.getPrimitiveTypeName should be (INT32)
+        _.getPrimitiveType.getPrimitiveTypeName should be(INT32)
       }
       columns.filter(c => c.getPath().contains("simpleCol")).foreach {
-        _.getPrimitiveType.getPrimitiveTypeName should be (DOUBLE)
+        _.getPrimitiveType.getPrimitiveTypeName should be(DOUBLE)
       }
+    }
+
+    it("should get correct RelDataType") {
+      val columns = Array(
+        new ColumnDescriptor(Array("column_int"), PrimitiveTypeName.INT32, 1, 1),
+        new ColumnDescriptor(Array("column_bigint"), PrimitiveTypeName.INT64, 1, 1),
+        new ColumnDescriptor(Array("column_varchar"), PrimitiveTypeName.BINARY, 1, 1),
+        new ColumnDescriptor(Array("column_float"), PrimitiveTypeName.FLOAT, 1, 1),
+        new ColumnDescriptor(Array("column_double"), PrimitiveTypeName.DOUBLE, 1, 1)
+      )
+      val table = ParquetTranslatableTable("SAMPLES", columns)
+      val relDataType = table.getRowType(new JavaTypeFactoryImpl())
+      val intField = relDataType.getField("column_int", false, false)
+      intField.getName should be ("COLUMN_INT")
+      intField.getType.getSqlTypeName should be (SqlTypeName.INTEGER)
+      val bigIntField = relDataType.getField("column_bigint", false, false)
+      bigIntField.getName should be ("COLUMN_BIGINT")
+      bigIntField.getType.getSqlTypeName should be (SqlTypeName.BIGINT)
+      val varcharField = relDataType.getField("column_varchar", false, false)
+      varcharField.getName should be ("COLUMN_VARCHAR")
+      varcharField.getType.getSqlTypeName should be (SqlTypeName.VARCHAR)
+      val floatField = relDataType.getField("column_float", false, false)
+      floatField.getName should be ("COLUMN_FLOAT")
+      floatField.getType.getSqlTypeName should be (SqlTypeName.FLOAT)
+      val doubleField = relDataType.getField("column_double", false, false)
+      doubleField.getName should be ("COLUMN_DOUBLE")
+      doubleField.getType.getSqlTypeName should be (SqlTypeName.DOUBLE)
     }
   }
 }
